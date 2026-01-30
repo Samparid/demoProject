fc33579fca148fadd93d0013ac22c755e345d1c7
samrat <samrat.keshri.24@gmail.com>
2026-01-30 22:40:32 +0530

    remove one unwanted package

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -3,7 +3,6 @@ package base;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 public class BaseTest {
