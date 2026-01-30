bdbd875bbe08c979839c3f49474921c56310e0cd
samrat <samrat.keshri.24@gmail.com>
2026-01-30 22:38:48 +0530

    added selenium grid, docker

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -3,14 +3,16 @@ package base;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 public class BaseTest {
 
     protected WebDriver driver;
-
+    @Parameters("browser")
     @BeforeMethod
-    public void setUp() {
-        driver = DriverFactory.initDriver();
+    public void setUp(String browser) {
+        driver = DriverFactory.initDriver(browser);
         driver.get("https://naveenautomationlabs.com/opencart/index.php?route=common/home");
         driver.manage().window().maximize();
     }
--- src/test/java/base/DriverFactory.java
+++ src/test/java/base/DriverFactory.java
@@ -1,20 +1,50 @@
 package base;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
 
 public class DriverFactory {
 
     private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
-    public static WebDriver initDriver() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized");
+    public static WebDriver initDriver(String browser) {
+        try {
+            String runMode = System.getProperty("runMode", "local");
 
-        WebDriver webDriver = new ChromeDriver(options);
-        driver.set(webDriver);
-        return webDriver;
+            if (runMode.equalsIgnoreCase("grid")) {
+
+                if (browser.equalsIgnoreCase("chrome")) {
+                    ChromeOptions options = new ChromeOptions();
+                    options.addArguments("--start-maximized");
+                    options.addArguments("--headless=new");
+                    options.addArguments("--no-sandbox");
+                    options.addArguments("--disable-dev-shm-usage");
+                    driver.set(new RemoteWebDriver(
+                            new URL("http://localhost:4444/"), options));
+                } else if (browser.equalsIgnoreCase("firefox")) {
+                    FirefoxOptions options = new FirefoxOptions();
+                    driver.set(new RemoteWebDriver(
+                            new URL("http://localhost:4444/"), options));
+                }
+            } else {
+                // Local execution
+                if (browser.equalsIgnoreCase("chrome")) {
+                    driver.set(new org.openqa.selenium.chrome.ChromeDriver());
+                } else if (browser.equalsIgnoreCase("firefox")) {
+                    driver.set(new org.openqa.selenium.firefox.FirefoxDriver());
+                }
+            }
+
+            getDriver().manage().window().maximize();
+            return getDriver();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static WebDriver getDriver() {
@@ -22,9 +52,7 @@ public class DriverFactory {
     }
 
     public static void quitDriver() {
-        if (driver.get() != null) {
-            driver.get().quit();
-            driver.remove();
-        }
+        getDriver().quit();
+        driver.remove();
     }
 }
